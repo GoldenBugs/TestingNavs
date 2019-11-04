e15420e7a6c413544a6fc1b087c69374b247864e
GoldenBugs <arizonaemmet@gmail.com>
2019-11-04 23:48:56 +0100

    first commit

---
+++ app/src/main/java/com/example/navigationcomponents/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.example.navigationcomponents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * This is my Login Screen
+ **/
+public class LoginActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_login);
+    }
+
+}
